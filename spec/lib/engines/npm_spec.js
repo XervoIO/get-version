b3e679555f982655d91a41bf4c2b3ad26e76adc1
Alex Padgett <padgett.alex@gmail.com>
2015-03-26 15:34:18 -0400

    Add npm engine module

---
+++ spec/lib/engines/npm_spec.js
@@ -0,0 +1,62 @@
+var expect     = require('chai').expect;
+var sinon      = require('sinon');
+var proxyquire = require('proxyquire');
+
+var requestMock    = sinon.stub();
+var versionFixture = require('../../fixtures/npm.json');
+
+var engine = proxyquire('../../../lib/engines/npm', {
+  'request': requestMock
+});
+
+describe('engines/npm', function () {
+  beforeEach(function () {
+    requestMock.yields(null, null, JSON.stringify(versionFixture));
+  });
+
+  afterEach(function () {
+    requestMock.reset();
+  });
+
+  describe('#resolveVersion', function () {
+    describe('when no version is specified', function () {
+      it('returns the latest version of npm', function (done) {
+        engine.resolveVersion({ engines: {}})
+          .then(function(version) {
+            expect(version).to.equal('2.7.3');
+            done();
+          });
+      });
+    });
+
+    describe('when a valid version is specified', function () {
+      it('returns the specified version', function (done) {
+        var opts = { engines: { 'npm': '1.1.25' }};
+        engine.resolveVersion(opts)
+          .then(function(version) {
+            expect(version).to.equal('1.1.25');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('#fetchVersions', function () {
+    it('retrieves a list of all versions of npm', function (done) {
+      engine.fetchVersions().then(function (versions) {
+        expect(versions.all).to.have.members(Object.keys(versionFixture.versions));
+        done();
+      });
+    });
+
+    it('retrieves the latest version of npm', function (done) {
+      engine.fetchVersions().then(function (versions) {
+        expect(versions.latest).to.equal('2.7.3');
+        done();
+      });
+    });
+  });
+
+  describe('#resolveVersion', function () {
+  });
+});
