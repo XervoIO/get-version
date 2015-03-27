88b13ac35572a6bcde5f859b6b7d812dce705624
Alex Padgett <padgett.alex@gmail.com>
2015-03-27 14:38:03 -0400

    Add iojs engine

---
+++ spec/lib/engines/iojs_spec.js
@@ -0,0 +1,89 @@
+var expect     = require('chai').expect;
+var proxyquire = require('proxyquire');
+var Q          = require('q');
+
+var versionServiceMock = {
+  getIojsVersions: function () {
+    var q = Q.defer();
+    q.resolve({
+      all: ['1.0.0', '1.0.1', '1.0.2', '1.0.3', '1.0.4', '1.1.0', '1.2.0', '1.3.0', '1.4.1', '1.4.2', '1.4.3', '1.5.0', '1.5.1', '1.6.0', '1.6.1', '1.6.2'],
+      latest: '1.6.2'
+    });
+    return q.promise;
+  }
+};
+
+var engine = proxyquire('../../../lib/engines/iojs', {
+  '../versions': versionServiceMock
+});
+
+describe('engines/iojs', function () {
+
+  describe('#resolveVersion', function () {
+    describe('when no version is specified', function () {
+      it('returns the latest version', function (done) {
+        engine.resolveVersion({ engines: {}})
+          .then(function(version) {
+            expect(version).to.equal('1.6.2');
+            done();
+          });
+      });
+    });
+
+    describe('when a valid version is specified', function () {
+      it('returns the specified version', function (done) {
+        var opts = { engines: { 'iojs': '1.6.0' }};
+        engine.resolveVersion(opts)
+          .then(function(version) {
+            expect(version).to.equal('1.6.0');
+            done();
+          });
+      });
+    });
+
+    describe('when an asterisk is provided', function () {
+      it('returns the latest version', function (done) {
+        var opts = { engines: { 'iojs': '*' }};
+        engine.resolveVersion(opts)
+          .then(function(version) {
+            expect(version).to.equal('1.6.2');
+            done();
+          });
+      });
+    });
+
+    describe('when a tilde range is provided', function () {
+      it('resolves the appropriate semantic version', function (done) {
+        var opts = { engines: { 'iojs': '~1.0' }};
+        engine.resolveVersion(opts)
+          .then(function(version) {
+            expect(version).to.equal('1.0.4');
+            done();
+          });
+      });
+    });
+
+    describe('when an caret range is provided', function () {
+      it('resolves the appropriate semantic version', function (done) {
+        var opts = { engines: { 'iojs': '^1.0.0' }};
+        engine.resolveVersion(opts)
+          .then(function(version) {
+            expect(version).to.equal('1.6.2');
+            done();
+          });
+      });
+    });
+
+    describe('when an x-version is provided', function () {
+      it('resolves the appropriate semantic version', function (done) {
+        var opts = { engines: { 'iojs': '1.x' }};
+        engine.resolveVersion(opts)
+          .then(function(version) {
+            expect(version).to.equal('1.6.2');
+            done();
+          });
+      });
+    });
+  });
+
+});
