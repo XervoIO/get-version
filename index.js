7acac3ebef1b2c41402b7fe19314438abb3ccf5a
Alex Padgett <padgett.alex@gmail.com>
2015-03-30 15:26:39 -0400

    Refactor to binary and commonjs

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-var argv    = require('minimist')(process.argv.slice(2));
-var path    = require('path');
 var engines = require('./lib/engines');
-var pkg     = require('./lib/package');
 var assert  = require('assert');
-var fs      = require('fs');
-var os      = require('os');
+var pkg     = require('./lib/package');
 
-assert(argv.engine, 'Must specify engine');
-assert(argv._[0], 'Must specify package');
-assert(Object.keys(engines).indexOf(argv.engine) !== -1, 'Invalid engine');
+module.exports = function (engStr, pkgFile) {
 
-var pkgFile = path.join(__dirname, argv._[0]);
-assert(fs.existsSync(pkgFile), 'Invalid package path');
+  assert(pkgFile, 'Must specify package file');
+  var pkgData = pkg.read(pkgFile);
 
-var engine = engines[argv.engine];
-var pkgData = pkg.read(path.join(__dirname, argv._[0]));
+  assert(pkgData, 'Invalid package file specified');
+  assert(engStr, 'Must specify engine');
+  assert(Object.keys(engines).indexOf(engStr) !== -1, 'Invalid engine');
 
-engine
-  .resolveVersion({ engines: pkgData.engines })
-  .then(function (version) {
-    process.stdout.write(version + os.EOL);
-  });
+  return engines[engStr].resolveVersion({ engines: pkgData.engines });
+};
